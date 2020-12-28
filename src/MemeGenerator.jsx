c2b3ada35e2927219e7f83b8f89d311d95118e34
afema <afema13@gmail.com>
2020-12-28 22:40:10 +0300

    new app

---
+++ src/MemeGenerator.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+
+class MemeGenerator extends Component {
+    constructor() {
+        super()
+        this.state = {
+            topText: "",
+            bottomText: "",
+            randomImage: "http://i.imgflip.com/1bij.jpg",
+            allMemeImgs: []
+        }
+    }
+    componentDidMount() {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(response => response.json())
+            .then(response => {
+                const { memes } = response.data
+                console.log(memes[0])
+                this.setState({ allMemeImgs: memes })
+            })
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange(event) {
+        const { name, value } = event.target
+        this.setState({ [name]: value })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+        const randNum = Math.floor(Math.random() * this.state.allMemeImgs.length)
+        const randMemeImg = this.state.allMemeImgs[randNum].url
+        this.setState({ randomImg: randMemeImg })
+    }
+
+    render() {
+        return (
+            <div>
+                <form className="meme-form" onSubmit={this.handleSubmit}>
+                    <input type="text" value={this.state.topText} name="topText" placeholder="Top Text" onChange={this.handleChange} />
+                    <input type="text" value={this.state.bottomText} name="bottomText" placeholder="Bottom Text" onChange={this.handleChange} />
+                    <button>Gen</button>
+                </form>
+                <div className="meme">
+                    <img align="center" src={this.state.randomImg} alt="" />
+                    <h2 className="top">{this.state.topText}</h2>
+                    <h2 className="bottom">{this.state.bottomText}</h2>
+                </div>
+            </div >
+        )
+    }
+}
+
+export default MemeGenerator
